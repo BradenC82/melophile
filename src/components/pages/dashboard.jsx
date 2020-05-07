87d99d314c5527d17b354722eaf73d3c6420f422
Braden Collingwood <bradencollingwood82@gmail.com>
2020-05-06 21:18:42 -0400

    Authorization Flow using Firebase Functions

--- src/components/pages/dashboard.jsx
+++ src/components/pages/dashboard.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import queryString from 'query-string';
 
-export default function dashboard() {
+export default function Dashboard() {
+    
+    const [name,setName] = useState('');
+    const [tracks, setTracks] = useState('');
+
+    useEffect(() => {
+        let parsed = queryString.parse(window.location.search.slice(1));
+        let accessToken = parsed.access_token;
+        
+        console.log(accessToken);
+        
+        axios.get('https://api.spotify.com/v1/me', {
+            headers: { 'Authorization': 'Bearer ' + accessToken }
+        }).then(response => {
+            if(response){
+                setName(response.data.display_name)
+            }
+        }
+        )
+
+        axios.get('https://api.spotify.com/v1/me/tracks', {
+            headers: { 'Authorization': 'Bearer ' + accessToken }
+        }).then(response => {
+            if(response){
+                setTracks(response.data.items)
+            }
+        }
+        )
+
+    },[])
+    
     return (
         <div>
             Dashboard
+            <p>{name}</p>
         </div>
     )
 }
